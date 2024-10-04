529eca881822d901d85f61c86ece6d7a4ee28d10
tanakorn31452 <tanakorn.te@gmail.com>
2024-10-04 11:45:53 +0700

    save for safe

--- scripts/rosbridge_connector.py
+++ scripts/rosbridge_connector.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python3
+#! /usr/bin/python3
 
 import time
-
 import rospy
 import roslibpy
 from autoware_msgs.msg import VehicleCmd
 from sensor_msgs.msg import Joy
+from std_msgs.msg import String, Float64
+from nav_msgs.msg import Odometry
+from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
+from control_msgs.msg import JointTrajectoryControllerState
+from std_srvs.srv import Trigger, TriggerResponse
 
 class RosBridgeConnector():
     def __init__(self):
@@ -13,18 +17,34 @@ class RosBridgeConnector():
         # ROS Subscriber
         self.vehicle_cmd_subscriber = rospy.Subscriber('/vehicle_cmd', VehicleCmd, self.vehicle_cmd_callback, queue_size=1)
         self.joy_subscriber = rospy.Subscriber('/joy', Joy, self.joy_callback, queue_size=1)
-
+        self.brake_command_subscriber = rospy.Subscriber('/brake_cmd', Float64, self.brake_command_callback, queue_size=1)
+        # ROS Publisher
+        self.odometry_publisher = rospy.Publisher('/odometry', Odometry, queue_size=50)
+        self.diagnostic_publisher = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=50)
+        self.joint_trajectory_controller_state_publisher = rospy.Publisher('/joint_trajectory_controller_state', JointTrajectoryControllerState, queue_size=50)
+        # ROS Service Server
+        self.set_zero_service_server = rospy.Service('/set_zero_position', Trigger, self.set_zero_position_service_server)
         # ROSBridge Websocket Instantiation
         self.ws_host = rospy.get_param("~host", "127.0.0.1")
         self.ws_port = rospy.get_param("~port", "9090")
         self.ws_reconnection_period = rospy.get_param("~reconnection_period", 1)
         self.ws_client = roslibpy.Ros(host=self.ws_host, port=self.ws_port)
+        # ROSBridge Websocket Subscriber
+        self.talker_listener = roslibpy.Topic(self.ws_client, '/chatter', 'std_msgs/String')
+        self.ws_odometry_subscriber = roslibpy.Topic(self.ws_client, '/odometry', 'nav_msgs/Odometry')
+        self.ws_diagnostic_subscriber = roslibpy.Topic(self.ws_client, '/diagnostic', 'diagnostic_msgs/DiagnosticArray')
+        self.ws_joint_trajectory_controller_state_subscriber = roslibpy.Topic(self.ws_client, '/joint_trajectory_controller_state', 'control_msgs/JointTrajectoryControllerState')
         # ROSBridge Websocket Publisher
         self.ws_vehicle_cmd_publisher = roslibpy.Topic(self.ws_client, "/nav_vel", "geometry_msgs/TwistStamped")
         self.ws_joy_publisher = roslibpy.Topic(self.ws_client, "/joy", "sensor_msgs/Joy")
-        
+        self.ws_brake_command_publisher = roslibpy.Topic(self.ws_client, "/brake_cmd", "std_msgs/Float64")
+        # ROSBridge Websocket Service Request
+        self.ws_service_request = roslibpy.Service(self.ws_client, '/set_zero_position', 'std_srvs/Trigger')
         # Private for checking reconnection
         self._timestamp = 0
+        self._response_message = None
+        self._success_status = None
+        self._is_ws_service_server_correct_response = False
 
     def connect(self):
         try:
@@ -33,11 +53,22 @@ class RosBridgeConnector():
         except Exception as e:
             rospy.logerr("Unable to connect to server! %s", e)
 
+    def websocket_subscriber(self):
+        try:
+            self.talker_listener.subscribe(self.talker_listener_callback)
+            self.ws_odometry_subscriber.subscribe(self.ws_odometry_subscriber_callback)
+            self.ws_diagnostic_subscriber.subscribe(self.ws_diagnostic_subscriber_callback)
+            self.ws_joint_trajectory_controller_state_subscriber.subscribe(self.ws_joint_trajectory_controller_state_subscriber_callback)
+            rospy.loginfo("Success for subscribe")
+        except Exception as e:
+            rospy.logerr("Unable to subscribe from server! %s", e)  
+    
     def advertise_topics(self):
         try:
             if self.ws_client.is_connected:
                 self.ws_vehicle_cmd_publisher.advertise()
                 self.ws_joy_publisher.advertise()
+                self.ws_brake_command_publisher.advertise()
                 rospy.loginfo("Topics are successfully advertised")
             else:
                 rospy.logerr("Unable to advertise the topic sinces client is not connected to the server yet")
@@ -116,17 +147,105 @@ class RosBridgeConnector():
         except Exception as e:
             rospy.logerr("Unable to publish message! %s", e)
 
+    def brake_command_callback(self, data):
+        try:
+            if self.ws_client.is_connected:
+                brake_json = roslibpy.Message(
+                    {
+                        "data": data.data
+                    }
+                )
+                self.ws_brake_command_publisher.publish(brake_json)
+            else:
+                rospy.logerr("Server is not connected")
+        except Exception as e:
+            rospy.logerr("Unable to publish message! %s", e)
+
+    def talker_listener_callback(self, data):
+        rospy.loginfo("Data: %s", data)
+    
+    def ws_odometry_subscriber_callback(self, data):
+        odom = Odometry()
+        odom.child_frame_id = data['child_frame_id']
+        odom.pose.pose.position.x = data['pose']['pose']['position']['x']
+        odom.pose.pose.position.y = data['pose']['pose']['position']['y']
+        odom.pose.pose.position.z = data['pose']['pose']['position']['z']
+        odom.pose.pose.orientation.x = data['pose']['pose']['orientation']['x']
+        odom.pose.pose.orientation.y = data['pose']['pose']['orientation']['y']
+        odom.pose.pose.orientation.z = data['pose']['pose']['orientation']['z']
+        odom.pose.pose.orientation.w = data['pose']['pose']['orientation']['w']
+        odom.pose.covariance = data['pose']['covariance']
+        odom.twist.twist.linear.x = data['twist']['twist']['linear']['x']
+        odom.twist.twist.linear.y = data['twist']['twist']['linear']['y']
+        odom.twist.twist.linear.z = data['twist']['twist']['linear']['z']
+        odom.twist.twist.angular.x = data['twist']['twist']['angular']['x']
+        odom.twist.twist.angular.x = data['twist']['twist']['angular']['y']
+        odom.twist.twist.angular.x = data['twist']['twist']['angular']['z']
+        odom.twist.covariance = data['twist']['covariance']
+        self.odometry_publisher.publish(odom)
+
+    def ws_diagnostic_subscriber_callback(self, data):
+        diag_array = DiagnosticArray()
+        for component_name, component_data in data.items():
+            status = DiagnosticStatus()
+            status.name = component_name  
+            status.level = component_data["level"] 
+            status.message = component_data["message"] 
+            status.hardware_id = component_data["hardware_id"] 
+            status.values = [KeyValue(key=key, value=value) for key, value in component_data["values"].items()]
+            diag_array.status.append(status)
+        self.diagnostic_publisher.publish(diag_array)
+
+    def ws_joint_trajectory_controller_state_subscriber_callback(self, data):
+        joint_state_msg = JointTrajectoryControllerState()
+        joint_state_msg.joint_names = data["joint_names"]
+        joint_state_msg.desired.positions = data["desired"]["positions"]
+        joint_state_msg.desired.velocities = data["desired"]["velocities"]
+        joint_state_msg.desired.accelerations = data["desired"]["accelerations"]
+        joint_state_msg.desired.effort = data["desired"]["effort"]
+        joint_state_msg.actual.positions = data["actual"]["positions"]
+        joint_state_msg.actual.velocities = data["actual"]["velocities"]
+        joint_state_msg.actual.accelerations = data["actual"]["accelerations"]
+        joint_state_msg.actual.effort = data["actual"]["effort"]
+        joint_state_msg.error.positions = data["error"]["positions"]
+        joint_state_msg.error.velocities = data["error"]["velocities"]
+        joint_state_msg.error.accelerations = data["error"]["accelerations"]
+        joint_state_msg.error.effort = data["error"]["effort"]
+        self.joint_trajectory_controller_state_publisher.publish(joint_state_msg)
+
+    def set_zero_position_service_server(self, req):
+        response = TriggerResponse()
+        ws_request = roslibpy.ServiceRequest()
+        self.ws_service_request.call(ws_request, self.ws_service_callback, self.ws_service_error_callback)
+        if self._is_ws_service_server_correct_response:
+            response.success = self._success_status
+            response.message = self._response_message
+        else:
+            response.success = False
+            response.message = "The ws server is error"
+        return response
+
+    def ws_service_callback(self, response):
+        self._success_status = response['success']
+        self._response_message = response['message']
+        self._is_ws_service_server_correct_response = True
+
+    def ws_service_error_callback(self, error_message):
+        self._is_ws_service_server_correct_response = False
+
     def handle_reconnection(self):
         if time.time() - self._timestamp > self.ws_reconnection_period: 
             if not self.ws_client.is_connected:
                 rospy.logwarn("Unable to connect to the server! Retrying...")
                 self.connect()
+                self.websocket_subscriber()
                 self.advertise_topics()
             self._timestamp = time.time()
 
 def main():
     init = RosBridgeConnector()
     init.connect()
+    init.websocket_subscriber()
     init.advertise_topics()
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
